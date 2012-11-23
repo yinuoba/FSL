236fc420541cedf0b42b31f1ccd3e7221b44b235
Hoogle <zeng.xianghu@hotmail.com>
2012-11-23 23:34:14 +0800

    update the format of star.js in components

--- components/star.js
+++ components/star.js
@@ -1,11 +1,11 @@
 /** 
- * FSL (Feishang Javascript Library) 
+ * FSL (Feishang Javascript Library)
  * Copyright (c) 2012, All rights reserved.
  *
  * @fileOverview Feishang Javascript Library components
  * @version 1.0
  * @author <a href="mailto:zeng.xianghu@hotmail.com">Hoogle</a>、<a href="http://weibo.com/yinuoba">新浪微博(一诺吧)</a>)
- * 
+ *
  * @description 星级评分插件。设置不同的评分变化，星的状态发生变化，根据评分的高低，判断是哭脸还是笑脸，并可在设置评分后执行回调函数；也可通过参数传入评分并禁用鼠标时间，仅展示一个评分。
  */
 FS.extend(FS, function() {
@@ -52,35 +52,35 @@ FS.extend(FS, function() {
 				FS.html(pjObj, state[nowScore])
 			}
 		});
-	 */	
-	var star = function(options){
-		// 直接返回一个对应的实例
-		return new star.main(options)
-	}
-	star.main = function(options) {
-			// 将this赋给self，方便混淆
-			var self = this;
-			// 取出参数列表
-			self.parentObj = options.parentObj;
-			self.cryNum = options.cryNum || 2;
-			self.smile = options.smile || 'smile';
-			self.cry = options.cry || 'cry';
-			self.nowScore = options.nowScore ? options.nowScore : 0;
-			self.mouseAble = options.mouseAble === false ? false : true;
-			// 如果允许鼠标交互，则定义如下参数
-			if(self.mouseAble){
-				self.enterFun = options.enterFun || function(){};
-				self.leaveFun = options.leaveFun || function(){}	
-			}
-			self.setScoreFun = options.setScoreFun || function(){};
-			// 根据parentObj计算出星节点数组
-			self.allStar = FS.makeArray(self.parentObj.children);
-			// 实例创建即执行
-			self.init()
+	 */
+	var star = function(options) {
+			// 直接返回一个对应的实例
+			return new star.main(options)
 		}
+	star.main = function(options) {
+		// 将this赋给self，方便混淆
+		var self = this;
+		// 取出参数列表
+		self.parentObj = options.parentObj;
+		self.cryNum = options.cryNum || 2;
+		self.smile = options.smile || 'smile';
+		self.cry = options.cry || 'cry';
+		self.nowScore = options.nowScore ? options.nowScore : 0;
+		self.mouseAble = options.mouseAble === false ? false : true;
+		// 如果允许鼠标交互，则定义如下参数
+		if(self.mouseAble) {
+			self.enterFun = options.enterFun || function() {};
+			self.leaveFun = options.leaveFun || function() {}
+		}
+		self.setScoreFun = options.setScoreFun || function() {};
+		// 根据parentObj计算出星节点数组
+		self.allStar = FS.makeArray(self.parentObj.children);
+		// 实例创建即执行
+		self.init()
+	}
 	star.main.prototype = {
 		constructor: star.main,
-		init: function(){
+		init: function() {
 			var mine = this;
 			var allStar = mine.allStar,
 				length = allStar.length,
@@ -92,38 +92,38 @@ FS.extend(FS, function() {
 			mine.setScoreFun(mine.nowScore);
 
 			// 当允许鼠标交互，则定义交互模块，否则只设置星的状态
-			if(mouseAble){
+			if(mouseAble) {
 				// 给星节点数组添加事件
-				allStar.each(function(o,i){
+				allStar.each(function(o, i) {
 					// 鼠标移入和移出，星做出相应的变化
-					FS.hover(o,function(){
+					FS.hover(o, function() {
 						// 当前索引之后的节点设为正常星
-						var normalObj = allStar.slice(i+1);
-							mine.setNormal(normalObj);
+						var normalObj = allStar.slice(i + 1);
+						mine.setNormal(normalObj);
 						// 鼠标移到星上，根据当前索引数给星加笑脸或哭脸
-						mine.setMode(i+1);
+						mine.setMode(i + 1);
 
 						// 鼠标移入的回调函数
-						mine.enterFun(i+1)
-					},function(){
+						mine.enterFun(i + 1)
+					}, function() {
 						var nowScore = mine.nowScore;
 						// mouseleave 根据nowScore设置笑脸或哭脸
 						mine.setMode(nowScore);
 
 						// 设置正常星
-						if(nowScore){
+						if(nowScore) {
 							var normalObj = allStar.slice(nowScore);
 							mine.setNormal(normalObj)
-						}else{
+						} else {
 							mine.setNormal(allStar)
 						}
 
 						// 鼠标一开后的回调函数
-						mine.leaveFun(i+1)
+						mine.leaveFun(i + 1)
 					})
 
 					// 鼠标点击星，改变this.nowScore,并改变状态
-					FS.addEvent(o, 'click', function(e){
+					FS.addEvent(o, 'click', function(e) {
 						mine.nowScore = i + 1;
 						// 设置
 						mine.setScoreFun(mine.nowScore)
@@ -131,40 +131,40 @@ FS.extend(FS, function() {
 				})
 			}
 		},
-		setMode: function(num){	// 跟据当前星数设置星的状态
+		setMode: function(num) { // 跟据当前星数设置星的状态
 			var mine = this,
 				allStar = mine.allStar;
 			// 分数为0，则全部设置为正常状态
-			if(!num){
+			if(!num) {
 				mine.setNormal(allStar);
 				return
 			}
 			var cryNum = mine.cryNum;
 			// 排除scoreNum=0后根据当前分数截取星
-			var arr = allStar.slice(0,num);
-			if(num>cryNum){
+			var arr = allStar.slice(0, num);
+			if(num > cryNum) {
 				this.setSmile(arr)
-			}else{
+			} else {
 				this.setCry(arr)
 			}
 		},
-		setNormal: function(arr){	// 将笑脸或哭脸设置成正常的星
+		setNormal: function(arr) { // 将笑脸或哭脸设置成正常的星
 			if(!arr[0]) return;
-			arr.each(function(o){
+			arr.each(function(o) {
 				FS.setClass(o, '')
 			})
 		},
-		setSmile: function(arr){	// 将节点数组中的星设置为笑脸
+		setSmile: function(arr) { // 将节点数组中的星设置为笑脸
 			if(!arr[0]) return;
 			var smileClass = this.smile;
-			arr.each(function(o){
+			arr.each(function(o) {
 				FS.setClass(o, smileClass)
 			})
 		},
-		setCry: function(arr){	// 将节点数组中的星设置为哭脸
+		setCry: function(arr) { // 将节点数组中的星设置为哭脸
 			if(!arr[0]) return;
 			var cryClass = this.cry;
-			arr.each(function(o){
+			arr.each(function(o) {
 				FS.setClass(o, cryClass)
 			})
 		}
